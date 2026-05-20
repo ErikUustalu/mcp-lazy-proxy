03abdcd1517341c62770149768868af9403cfde0
Erik Uustalu <erik.uustalu@gmail.com>
2026-05-20 19:23:34 +0300

    Added debug code to proxy.py

--- proxy.py
+++ proxy.py
@@ -38,12 +38,38 @@ class Proxy:
         for server in self.config["mcp_servers"]:
             await server["client"].__aexit__(None, None, None)
 
-if __name__ == "__main__":
+async def main():
     proxy = Proxy()
-    asyncio.run(proxy.connect())
-    print("Available tools:", asyncio.run(proxy.list_tools()))
-    tool_name = input("Enter tool name to describe: ")
-    print(asyncio.run(proxy.describe_tool(tool_name)))
-    tool_name = input("Enter tool name to call: ")
-    args = input("Enter arguments: ")
-    print(asyncio.run(proxy.call_tool(tool_name, args)))
+    await proxy.connect()
+    
+    while True:
+        task = input("list(1), describe(2), call(3), exit(4): ")
+        if task == "1":
+            tools = await proxy.list_tools()
+            for i in range(len(tools)):
+                print(f"[{i}] {tools[i]}")
+
+        elif task == "2":
+            tool_name = input("Tool name: ")
+            if tool_name.isdigit():
+                tool_name = (await proxy.list_tools())[int(tool_name)]
+            print(await proxy.describe_tool(tool_name))
+
+        elif task == "3":
+            tool_name = input("Tool name: ")
+            if tool_name.isdigit():
+                tool_name = (await proxy.list_tools())[int(tool_name)]
+            args = input("Arguments: ")
+            if args == "":
+                args = "{}"
+            print(await proxy.call_tool(tool_name, json.loads(args)))
+
+        elif task == "4":
+            await proxy.disconnect()
+            break
+
+        else:
+            print("Invalid command")
+
+if __name__ == "__main__":
+    asyncio.run(main())
